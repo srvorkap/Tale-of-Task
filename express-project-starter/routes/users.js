35a7e328d4f9fb4b6d2280ed60d26260328cca16
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2021-11-15 17:36:47 -0500

    Setup user register get route

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
+const { csrfProtection, asyncHandler, bcrypt }= require('./utils')
+const {User} = require('../db/models');
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/signup', csrfProtection,  asyncHandler(async(req, res, next) => {
+  const user = await User.build()
+  res.render('user-signup', {
+    csrfToken: req.csrfToken(),
+    user,
+    title: "Sign Up"
+  });
+}));
 
 module.exports = router;
