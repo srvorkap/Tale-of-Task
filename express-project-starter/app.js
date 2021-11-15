3c47b5fbcc4d510626787fdd0288aeeec3256e1b
John J. Lee <johnlee1120@gmail.com>
2021-11-15 15:30:09 -0800

    Finish user sign-up post route

--- express-project-starter/app.js
+++ express-project-starter/app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const {sessionSecret} = require('./config/index')
+const { sessionSecret } = require('./config/index')
 
 const app = express();
 
