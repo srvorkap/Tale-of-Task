464b80b49b32ff267d3cd862ca19d0ad15c9e966
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2021-11-17 17:31:35 -0500

    addFormUpdatePopup

--- public/javascripts/api-task.js
+++ public/javascripts/api-task.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     addCreateFunction()
 
     const updateButtons = document.querySelectorAll('.update-task-btn')
-
+    updateButtons.forEach(button => addUpdateFunction(button))
     const deleteButtons = document.querySelectorAll('.delete-task-btn')
     deleteButtons.forEach(button => addDeleteFunction(button));
 })
@@ -83,6 +83,7 @@ const addCreateFunction = () => {
             // console.log(ul);
 
             addDeleteFunction(deleteBtn);
+            addUpdateFunction(updateBtn)
         }
 
         textBox.value = null;
@@ -117,4 +118,141 @@ const addDeleteFunction = (button) => {
             container.remove()
         }
     })
+}
+
+const addSaveFunction = (button) => {
+
+    button.addEventListener('click', async(ev) => {
+        const taskId = ev.target.id.split('-')[1]
+
+
+
+        const res = await fetch(`/tasks/${taskId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify()
+        })
+
+    })
+
+
+}
+
+const addUpdateFunction = (button) => {
+    button.addEventListener('click', async(ev) => {
+        ev.preventDefault();
+        const taskId = ev.target.id.split('-')[1];
+         const res = await fetch(`/tasks/${taskId}`, {
+            method: "GET"
+         })
+
+         const data = res.json()
+
+         const {description, dueDate, estimatedTime, importance} = data;
+
+         const minutes = estimatedTime % 60;
+         const hours = Math.floor(estimatedTime / 60);
+
+         const form = document.createElement('form');
+         const textDiv = document.createElement('div');
+         const dataDiv = document.createElement('div');
+         const errorsDiv = document.createElement('div');
+         const dueDateLabel = document.createElement('label');
+         const timeLabel = document.createElement('label');
+         const hoursLabel = document.createElement('label');
+         const minutesLabel = document.createElement('label');
+         const textInput = document.createElement('input');
+         const dueDateInput = document.createElement('input');
+         const hoursInput = document.createElement('input');
+         const minutesInput = document.createElement('input');
+         const select = document.createElement('select');
+         const optionSelect = document.createElement('option');
+         const optionNone = document.createElement('option');
+         const optionHigh = document.createElement('option');
+         const optionMed = document.createElement('option');
+         const optionLow = document.createElement('option');
+         const errorsDisplay = document.createElement('ul')
+         const saveButton = document.createElement('button');
+
+         textInput.type= 'text';
+         textInput.name= 'description';
+         textInput.id= `text-box-${taskId}`;
+         textInput.value= description;
+
+         dueDateLabel.for = 'dueDate';
+         dueDateLabel.innerText= 'Due Date';
+         dueDateInput.type = 'datetime-local';
+         dueDateInput.id = `dueDate-${taskId}`;
+         dueDateInput.value = dueDate;
+
+         timeLabel.innerText= 'Estimated Time';
+         hoursLabel.for = 'hours';
+         hoursLabel.innerText = "Hours";
+         hoursInput.type = 'number';
+         hoursInput.name = 'hours'
+         hoursInput.id= `hours-${taskId}`
+         hoursInput.max= 24;
+         hoursInput.min = 0;
+         hoursInput.value = hours;
+
+
+         minutesLabel.for = 'minutes';
+         minutesLabel.innerText = "Minutes";
+         minutesInput.type = 'number';
+         minutesInput.name = 'minutes';
+         minutesInput.id= `minutes-${taskId}`;
+         minutesInput.max= 60;
+         minutesInput.min = 0;
+         minutesInput.value = minutes;
+
+
+         select.name = 'importance';
+         select.id = `importance-${taskId}`;
+         optionSelect.innerText = "-- Select Priority --";
+         optionNone.innerText = "None";
+         optionNone.value = 0;
+         optionHigh.innerText = "High";
+         optionHigh.value = 3;
+         optionMed.innerText = "Medium";
+         optionMed.value = 2;
+         optionLow.innerText = "Low";
+         optionLow.value = 1;
+
+         saveButton.innerText = "Save";
+         saveButton.id = `save-${taskId}`
+
+         errorsDisplay.id = `errors-${taskId}`;
+         errorsDiv.appendChild(errorsDisplay);
+
+         select.appendChild(optionSelect)
+         select.appendChild(optionNone)
+         select.appendChild(optionHigh)
+         select.appendChild(optionMed)
+         select.appendChild(optionLow)
+
+         textDiv.appendChild(textInput)
+
+         dataDiv.appendChild(dueDateLabel)
+         dataDiv.appendChild(dueDateInput)
+         dataDiv.appendChild(timeLabel)
+         dataDiv.appendChild(hoursLabel)
+         dataDiv.appendChild(hoursInput)
+         dataDiv.appendChild(minutesLabel)
+         dataDiv.appendChild(minutesInput)
+         dataDiv.appendChild(select)
+         dataDiv.appendChild(saveButton)
+
+         form.appendChild(textDiv)
+         form.appendChild(dataDiv)
+         form.appendChild(errorsDiv)
+         console.log(form)
+
+
+         const taskListDiv = document.getElementById(`task-container-${taskId}`)
+         taskListDiv.appendChild(form)
+
+    })
+
 }
