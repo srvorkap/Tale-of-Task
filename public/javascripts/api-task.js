93a21bbade8ac980e81c9f33b5b4c1c73576f79a
John J. Lee <johnlee1120@gmail.com>
2021-11-17 15:05:22 -0800

    Add update functionality

--- public/javascripts/api-task.js
+++ public/javascripts/api-task.js
@@ -98,7 +98,6 @@ const addCreateFunction = () => {
             <option value=2> Medium </option>
             <option value=1> Low </option>
         </select>
-
         `
     })
 }
@@ -122,10 +121,12 @@ const addDeleteFunction = (button) => {
 
 const addSaveFunction = (button) => {
 
-    button.addEventListener('click', async(ev) => {
+    button.addEventListener('click', async (ev) => {
+        ev.preventDefault();
+
+        // Make the other buttons visible again
+
         const taskId = ev.target.id.split('-')[1]
-
-
 
         const res = await fetch(`/tasks/${taskId}`, {
             method: "PUT",
@@ -134,125 +135,129 @@ const addSaveFunction = (button) => {
             },
             body: JSON.stringify()
         })
-
     })
-
-
 }
 
 const addUpdateFunction = (button) => {
-    button.addEventListener('click', async(ev) => {
+    button.addEventListener('click', async (ev) => {
         ev.preventDefault();
+
         const taskId = ev.target.id.split('-')[1];
-         const res = await fetch(`/tasks/${taskId}`, {
+        console.log(taskId);
+        const taskListDiv = document.getElementById(`task-container-${taskId}`);
+        const divKids = taskListDiv.children;
+        for (let el of divKids) {
+            el.style.display = 'none';
+        }
+
+        const res = await fetch(`/tasks/${taskId}`, {
             method: "GET"
-         })
+        });
 
-         const data = res.json()
+        const data = res.json()
+        console.log(data);
 
-         const {description, dueDate, estimatedTime, importance} = data;
+        const { description, dueDate, estimatedTime, importance } = data;
 
-         const minutes = estimatedTime % 60;
-         const hours = Math.floor(estimatedTime / 60);
+        const minutes = estimatedTime % 60;
+        const hours = Math.floor(estimatedTime / 60);
 
-         const form = document.createElement('form');
-         const textDiv = document.createElement('div');
-         const dataDiv = document.createElement('div');
-         const errorsDiv = document.createElement('div');
-         const dueDateLabel = document.createElement('label');
-         const timeLabel = document.createElement('label');
-         const hoursLabel = document.createElement('label');
-         const minutesLabel = document.createElement('label');
-         const textInput = document.createElement('input');
-         const dueDateInput = document.createElement('input');
-         const hoursInput = document.createElement('input');
-         const minutesInput = document.createElement('input');
-         const select = document.createElement('select');
-         const optionSelect = document.createElement('option');
-         const optionNone = document.createElement('option');
-         const optionHigh = document.createElement('option');
-         const optionMed = document.createElement('option');
-         const optionLow = document.createElement('option');
-         const errorsDisplay = document.createElement('ul')
-         const saveButton = document.createElement('button');
+        const form = document.createElement('form');
+        const textDiv = document.createElement('div');
+        const dataDiv = document.createElement('div');
+        const errorsDiv = document.createElement('div');
+        const dueDateLabel = document.createElement('label');
+        const timeLabel = document.createElement('label');
+        const hoursLabel = document.createElement('label');
+        const minutesLabel = document.createElement('label');
+        const textInput = document.createElement('input');
+        const dueDateInput = document.createElement('input');
+        const hoursInput = document.createElement('input');
+        const minutesInput = document.createElement('input');
+        const select = document.createElement('select');
+        const optionSelect = document.createElement('option');
+        const optionNone = document.createElement('option');
+        const optionHigh = document.createElement('option');
+        const optionMed = document.createElement('option');
+        const optionLow = document.createElement('option');
+        const errorsDisplay = document.createElement('ul')
+        const saveButton = document.createElement('button');
 
-         textInput.type= 'text';
-         textInput.name= 'description';
-         textInput.id= `text-box-${taskId}`;
-         textInput.value= description;
+        textInput.type = 'text';
+        textInput.name = 'description';
+        textInput.id = `text-box-${taskId}`;
+        textInput.value = description;
 
-         dueDateLabel.for = 'dueDate';
-         dueDateLabel.innerText= 'Due Date';
-         dueDateInput.type = 'datetime-local';
-         dueDateInput.id = `dueDate-${taskId}`;
-         dueDateInput.value = dueDate;
+        dueDateLabel.for = 'dueDate';
+        dueDateLabel.innerText = 'Due Date';
+        dueDateInput.type = 'datetime-local';
+        dueDateInput.id = `dueDate-${taskId}`;
+        dueDateInput.value = dueDate;
 
-         timeLabel.innerText= 'Estimated Time';
-         hoursLabel.for = 'hours';
-         hoursLabel.innerText = "Hours";
-         hoursInput.type = 'number';
-         hoursInput.name = 'hours'
-         hoursInput.id= `hours-${taskId}`
-         hoursInput.max= 24;
-         hoursInput.min = 0;
-         hoursInput.value = hours;
+        timeLabel.innerText = 'Estimated Time';
 
+        hoursLabel.for = 'hours';
+        hoursLabel.innerText = "Hours";
+        hoursInput.type = 'number';
+        hoursInput.name = 'hours'
+        hoursInput.id = `hours-${taskId}`
+        hoursInput.max = 24;
+        hoursInput.min = 0;
+        hoursInput.value = hours;
 
-         minutesLabel.for = 'minutes';
-         minutesLabel.innerText = "Minutes";
-         minutesInput.type = 'number';
-         minutesInput.name = 'minutes';
-         minutesInput.id= `minutes-${taskId}`;
-         minutesInput.max= 60;
-         minutesInput.min = 0;
-         minutesInput.value = minutes;
+        minutesLabel.for = 'minutes';
+        minutesLabel.innerText = "Minutes";
+        minutesInput.type = 'number';
+        minutesInput.name = 'minutes';
+        minutesInput.id = `minutes-${taskId}`;
+        minutesInput.max = 60;
+        minutesInput.min = 0;
+        minutesInput.value = minutes;
 
+        select.name = 'importance';
+        select.id = `importance-${taskId}`;
+        optionSelect.innerText = "-- Select Priority --";
+        optionNone.innerText = "None";
+        optionNone.value = 0;
+        optionHigh.innerText = "High";
+        optionHigh.value = 3;
+        optionMed.innerText = "Medium";
+        optionMed.value = 2;
+        optionLow.innerText = "Low";
+        optionLow.value = 1;
 
-         select.name = 'importance';
-         select.id = `importance-${taskId}`;
-         optionSelect.innerText = "-- Select Priority --";
-         optionNone.innerText = "None";
-         optionNone.value = 0;
-         optionHigh.innerText = "High";
-         optionHigh.value = 3;
-         optionMed.innerText = "Medium";
-         optionMed.value = 2;
-         optionLow.innerText = "Low";
-         optionLow.value = 1;
+        saveButton.innerText = "Save";
+        saveButton.id = `save-${taskId}`;
+        addSaveFunction(saveButton);
 
-         saveButton.innerText = "Save";
-         saveButton.id = `save-${taskId}`
+        errorsDisplay.id = `errors-${taskId}`;
+        errorsDiv.appendChild(errorsDisplay);
 
-         errorsDisplay.id = `errors-${taskId}`;
-         errorsDiv.appendChild(errorsDisplay);
+        select.appendChild(optionSelect);
+        select.appendChild(optionNone);
+        select.appendChild(optionHigh);
+        select.appendChild(optionMed);
+        select.appendChild(optionLow);
 
-         select.appendChild(optionSelect)
-         select.appendChild(optionNone)
-         select.appendChild(optionHigh)
-         select.appendChild(optionMed)
-         select.appendChild(optionLow)
+        textDiv.appendChild(textInput);
 
-         textDiv.appendChild(textInput)
+        dataDiv.appendChild(dueDateLabel);
+        dataDiv.appendChild(dueDateInput);
+        dataDiv.appendChild(timeLabel);
+        dataDiv.appendChild(hoursLabel);
+        dataDiv.appendChild(hoursInput);
+        dataDiv.appendChild(minutesLabel);
+        dataDiv.appendChild(minutesInput);
+        dataDiv.appendChild(select);
+        dataDiv.appendChild(saveButton);
 
-         dataDiv.appendChild(dueDateLabel)
-         dataDiv.appendChild(dueDateInput)
-         dataDiv.appendChild(timeLabel)
-         dataDiv.appendChild(hoursLabel)
-         dataDiv.appendChild(hoursInput)
-         dataDiv.appendChild(minutesLabel)
-         dataDiv.appendChild(minutesInput)
-         dataDiv.appendChild(select)
-         dataDiv.appendChild(saveButton)
+        form.appendChild(textDiv);
+        form.appendChild(dataDiv);
+        form.appendChild(errorsDiv);
 
-         form.appendChild(textDiv)
-         form.appendChild(dataDiv)
-         form.appendChild(errorsDiv)
-         console.log(form)
+        // console.log(form)
 
-
-         const taskListDiv = document.getElementById(`task-container-${taskId}`)
-         taskListDiv.appendChild(form)
-
+        taskListDiv.appendChild(form);
     })
 
 }
