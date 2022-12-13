505536b1079f395b2084eb9df78a719d19222607
Srdan <srdan.vorkapic@outlook.com>
2022-12-13 11:30:27 -0800

    remove ssl

--- config/database.js
+++ config/database.js
@@ -15,9 +15,5 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
-    ssl: true,
-    dialectOptions: {
-      "ssl": {"require":true}
-    }
   }
 };
